af9ef154c389edc5d7b4a7b406f5a2560273ea83
Charrath <Marco5142@googlemail.com>
2024-06-26 20:16:11 +0200

    questions designed

---
+++ JSON.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+      "question": "Was bedeutet die Abkürzung 'CPU'?",
+      "answer_1": "Central Processing Unit",
+      "answer_2": "Central Performance Unit",
+      "answer_3": "Computer Personal Unit",
+      "answer_4": "Central Program Unit",
+      "right_answer": 1
+    },
+    {
+      "question": "Welche der folgenden ist eine Programmiersprache?",
+      "answer_1": "HTML",
+      "answer_2": "Python",
+      "answer_3": "CSS",
+      "answer_4": "HTTP",
+      "right_answer": 2
+    },
+    {
+      "question": "Was ist ein Byte?",
+      "answer_1": "Eine Einheit für die Prozessorleistung",
+      "answer_2": "Eine Programmiersprache",
+      "answer_3": "Eine Speichergröße",
+      "answer_4": "Eine Netzwerktechnologie",
+      "right_answer": 3
+    },
+    {
+      "question": "Welche Tastenkombination kopiert eine markierte Datei oder Text?",
+      "answer_1": "Ctrl + C",
+      "answer_2": "Ctrl + V",
+      "answer_3": "Ctrl + X",
+      "answer_4": "Ctrl + Z",
+      "right_answer": 1
+    },
+    {
+      "question": "Welche Datei-Endung kennzeichnet eine ausführbare Datei unter Windows?",
+      "answer_1": ".txt",
+      "answer_2": ".exe",
+      "answer_3": ".jpg",
+      "answer_4": ".doc",
+      "right_answer": 2
+    },
+    {
+      "question": "Was bedeutet die Abkürzung 'RAM'?",
+      "answer_1": "Read Access Memory",
+      "answer_2": "Random Access Memory",
+      "answer_3": "Run Access Memory",
+      "answer_4": "Read Available Memory",
+      "right_answer": 2
+    },
+    {
+      "question": "Was ist eine Festplatte?",
+      "answer_1": "Ein Eingabegerät",
+      "answer_2": "Ein Ausgabegerät",
+      "answer_3": "Ein Speichermedium",
+      "answer_4": "Ein Netzwerkgerät",
+      "right_answer": 3
+    },
+    {
+      "question": "Welche der folgenden ist ein Webbrowser?",
+      "answer_1": "Windows",
+      "answer_2": "Linux",
+      "answer_3": "Google Chrome",
+      "answer_4": "MacOS",
+      "right_answer": 3
+    },
+    {
+      "question": "Was ist das Hauptziel einer Firewall?",
+      "answer_1": "Software zu beschleunigen",
+      "answer_2": "Hardware zu schützen",
+      "answer_3": "Netzwerke vor unerlaubtem Zugriff zu schützen",
+      "answer_4": "Dateien zu komprimieren",
+      "right_answer": 3
+    },
+    {
+      "question": "Welche der folgenden ist keine Programmiersprache?",
+      "answer_1": "Python",
+      "answer_2": "Java",
+      "answer_3": "HTML",
+      "answer_4": "C++",
+      "right_answer": 3
+    }
+  ];
